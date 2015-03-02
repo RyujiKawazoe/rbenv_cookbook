ddc7db3ac8fca84c49d33e22806b4be6912b751d
RyujiKawazoe <kawazoe@val.co.jp>
2015-03-02 18:50:16 +0900

    init

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+default['ruby-env']['user'] = "vagrant"
+default['ruby-env']['group'] = "vagrant"
+default['ruby-env']['version'] = "2.2.0"
+default['ruby-env']['rbenv_url'] = "https://github.com/sstephenson/rbenv"
+default['ruby-env']['ruby-build_url'] = "https://github.com/sstephenson/ruby-build"
+default['ruby-env']['rbenv']['branch'] = "master"
+default['ruby-env']['ruby-build']['branch'] = "master"
+
+#rbenv-gem-rehash
+default['ruby-env']['rbenv-gem-rehash_url'] = "https://github.com/sstephenson/rbenv-gem-rehash.git"
+
+#rbenv-default-gems
+default['ruby-env']['rbenv-default-gems_url'] = "https://github.com/sstephenson/rbenv-default-gems.git"
